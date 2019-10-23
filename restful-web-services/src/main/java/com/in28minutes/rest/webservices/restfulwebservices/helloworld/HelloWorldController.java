9c28d53e602e7a33523b6fa3610b6b646d46dd7e
Rapaka Siva Sai Santhi Swaroop <rapaka.sivasaisanthiswaroop@cognizant.com>
2019-10-23 16:53:05 +0530

    Step 84 Completed but not able to login to my application

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		//throw new RuntimeException("Some Error has Happenned!Contact Support at ***-***");
+		return new HelloWorldBean("Hello World - Changed");
+	}
+	
+	@GetMapping(path="/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World ,%s",name));
+	}
+}
